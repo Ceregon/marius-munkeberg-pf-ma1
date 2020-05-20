a2d104580d5e16d22840e34d4b8ba68c2afbd411
unknown <kingturgon@gmail.com>
2020-05-20 12:45:28 +0200

    My script file

---
+++ script.js
@@ -0,0 +1,30 @@
+//  question 1
+var firstVar;
+
+//  question 2
+var name = "Marius";
+
+//  question 3
+var number = 5;
+
+//  question 4
+var division = 20/5;
+
+//  question 5
+typeof("frog");
+
+//  question 6
+var orderHasShipped = true;
+
+//  question 7
+if (orderHasShipped === true){
+  console.log("true");
+}
+else {
+  console.log("false");
+}
+
+//  question 8
+for(var counter = 0; counter <=9; counter++){
+  console.log(counter);
+}
